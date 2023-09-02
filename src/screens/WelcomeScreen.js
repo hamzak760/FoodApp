4e9060b1c001a1ea822944de97f7150caeb099d8
hamzak760 <hamzak760@gmail.com>
2023-09-02 20:03:53 +0500

    welcome screen

--- src/screens/WelcomeScreen.js
+++ src/screens/WelcomeScreen.js
@@ -1,11 +1,22 @@
 import React from 'react'
+import { StyleSheet, Text, View, Image } from 'react-native'
 
 const WelcomeScreen = () => {
   return (
     <div>
-      
+      <View style={style.container}>
+        <Text>Welcome to Foodie</Text>
+        <View style={style.logoout}>
+          <Image />
+        </View>
+        <View style={hr80} />
+      </View>
     </div>
   )
 }
+
+const styles = StyleSheet.create({
+  conatiner: {}
+})
 
 export default WelcomeScreen
